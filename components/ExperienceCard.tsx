a2f53d483e56816f6cea810e8194bd78591893cc
Patrick Aigbogun <107092681+patrickaigbogun@users.noreply.github.com>
2024-04-23 20:32:25 +0100

    Update ExperienceCard.tsx

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,3 +1,4 @@
+//experiencecard.tsx
 import React from 'react'
 import { motion } from 'framer-motion'
 
@@ -5,9 +6,8 @@ type Props = {}
 
 function ExperienceCard({ }: Props) {
 	return (
-		<article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 
-    w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100
-    opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden '>
+		<article className='flex flex-col rounded-lg items-center space-y-7 max-w-3xl w-full snap-center bg-[#292929] p-10 hover:opacity-100
+opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
 			<motion.img
 				initial={{
 					y: -100,
@@ -23,16 +23,15 @@ function ExperienceCard({ }: Props) {
 				viewport={{
 					once: true
 				}}
-				className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px]
-            object-cover object-center'
+				className='w-32 h-32 rounded-full md:w-48 md:h-48 xl:w-[200px] xl:h-[200px] object-cover object-center'
 				src="https://media.gettyimages.com/id/515481284/photo/yoga-meditating-man-with-big-afro-hair.jpg?s=612x612&w=0&k=20&c=BYHe1wlnQ4zawN8PpO_Usc3hYMDTqYM3hXZq_VIIkxU="
 				alt="this will be your logo"
 			/>
 
 			<div className='px-0 md:px-10'>
-				<h4 className='text-4xl font-light'>CEO of VOSEKDBA</h4>
-				<p className='font-bold text-2xl mt-1'>VOSEKDBA</p>
-				<div className='flex space-x-2 my-2'>
+				<h4 className='text-3xl font-light md:text-4xl'>CEO of VOSEKDBA</h4>
+				<p className='font-bold text-xl md:text-2xl mt-1'>VOSEKDBA</p>
+				<div className='flex flex-wrap md:flex-row space-x-2 my-2'>
 					<img className='h-10 w-10 rounded-full'
 						src="../public/logo512.png" alt="technology usedtechnology used" />
 					<img className='h-10 w-10 rounded-full'
