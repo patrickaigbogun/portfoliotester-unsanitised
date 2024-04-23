927722b421584735e7c211e5e5b7b5f563ba98da
Patrick Aigbogun <107092681+patrickaigbogun@users.noreply.github.com>
2024-04-23 20:29:57 +0100

    Update Skill.tsx

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,3 +1,4 @@
+//skill.tsx
 import React from 'react'
 import { motion } from 'framer-motion'
 
@@ -16,14 +17,11 @@ function Skill({directionLeft}: Props) {
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
         src='public\logo512.png'
-        className=' rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32
-        xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out '
+        className='rounded-full border border-gray-500 object-cover w-16 md:w-20 xl:w-24 h-16 md:h-20 xl:h-24 filter group-hover:grayscale transition duration-300 ease-in-out'
         />
-        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300
-        ease-in-out group-hover:bg-white *:first-letter:h-24 w-24 md:h-28 xl:w-32 xl:h-32
-        rounded-full z-20'>
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-full h-full rounded-full z-20'>
             <div className='flex items-center justify-center h-full'>
-                <p className='text-3xl font-bold text-black opacity-100' >100%</p>
+                <p className='text-xl md:text-2xl xl:text-3xl font-bold text-black opacity-100'>100%</p>
             </div>
         </div>
     </div>
