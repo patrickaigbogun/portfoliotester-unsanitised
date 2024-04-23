640d2a859a7b741ca369c84308c654734ea8e589
Patrick Aigbogun <107092681+patrickaigbogun@users.noreply.github.com>
2024-04-23 20:36:24 +0100

    Update BackgroundCircles.tsx

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -1,9 +1,13 @@
+//backgroundcircle.tsx
 import React from 'react'
 import { motion } from "framer-motion";
+import { useMediaQuery } from 'react-responsive';
 
 type Props = {}
 
 function BackgroundCircles({}: Props) {
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+  
   return (
     <motion.div
     initial={{
@@ -18,10 +22,22 @@ function BackgroundCircles({}: Props) {
         duration: 2.5,
     }}
 
-    className='relative flex justify-center items-center p-0'>
-        <div className='absolute border border-[#333333]-500 rounded-full h-[200px] w-[200px] mt-52 animate-ping' />
-        <div className='absolute border border-[#333333]-500 rounded-full h-[500px] w-[500px] mt-52 ' />
-        <div className='absolute border border-green-500 rounded-full opacity-20 h-[650px] w-[650px] mt-52 animate-pulse' />
+    className='relative flex justify-center items-center p-0'
+    >
+      {isMobile ? (
+        // Smaller circles for mobile screens
+        <>
+          <div className='absolute border border-[#333333]-500 rounded-full h-[80px] w-[80px] mt-20 animate-ping' />
+          <div className='absolute border border-[#333333]-500 rounded-full h-[200px] w-[200px] mt-20 ' />
+        </>
+      ) : (
+        // Larger circles for larger screens
+        <>
+          <div className='absolute border border-[#333333]-500 rounded-full h-[200px] w-[200px] mt-52 animate-ping' />
+          <div className='absolute border border-[#333333]-500 rounded-full h-[500px] w-[500px] mt-52 ' />
+          <div className='absolute border border-green-500 rounded-full opacity-20 h-[650px] w-[650px] mt-52 animate-pulse' />
+        </>
+      )}
     </motion.div>
   )
 }
