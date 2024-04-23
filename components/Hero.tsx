9111c52edf2f6f5253617038e0b3a06f228e7ad9
Patrick Aigbogun <107092681+patrickaigbogun@users.noreply.github.com>
2024-04-23 20:31:11 +0100

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+//hero.tsx
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './BackgroundCircles'
@@ -17,22 +18,27 @@ export default function Hero({}: Props) {
         delaySpeed: 2000,
       })
   return (
-    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center overflow-hidden md:p-10 lg:p-20'>
+  {/* ... content */}
         <BackgroundCircles />
-        <img src='https://media.gettyimages.com/id/1382470869/photo/african-american-man-with-african-hairstyle-using-smart-phone.jpg?s=612x612&w=0&k=20&c=MGxBUVKI67swpU-T_pmJGG6HtvO1xCxUwpyBpX6ni7Q='
-        className='w-56 h-56 rounded-full object-cover justify-center '
+        <Image
+            src='https://media.gettyimages.com/id/1382470869/photo/african-american-man-with-african-hairstyle-using-smart-phone.jpg?s=612x612&w=0&k=20&c=MGxBUVKI67swpU-T_pmJGG6HtvO1xCxUwpyBpX6ni7Q='
+            alt='Hero Image'
+            className='rounded-full object-cover w-48 md:w-64 lg:w-80 h-48 md:h-64 lg:h-80'
         />
+
         
         <div className=' z-20 '>
             <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
                 Software developer
             </h2>
-            <h1>
-                <span>{text}</span> 
+            <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold tracking-[10px] md:tracking-[15px]'> 
+                <span>{text}</span>
                 <Cursor cursorColor="green" />
             </h1>
+
             
-            <div>
+            <div className='flex space-x-4 md:space-x-8 lg:space-x-12 justify-center'>
                 <Link href='#about'>
                     <button className='heroButton'>About</button>
                 </Link>
@@ -44,12 +50,8 @@ export default function Hero({}: Props) {
                 </Link>
                 <Link href='#projects'>
                     <button className='heroButton'>Projects</button>
-                </Link>
-            
-           
-            
-            
-        </div>
+                </Link>    
+            </div>
 
         </div>
     </div>
